d9845df8deed9720df76a2d4069eb698dbc7636a
Maajid Siddiqui <msiddiqui.digitals@gmail.com>
2020-02-19 16:21:13 +0500

    Done some changes in day06

--- Day06/timeline-app/src/data.js
+++ Day06/timeline-app/src/data.js
@@ -12,18 +12,6 @@ const data = [
       }
     },
     {
-      "text": "Dolore veniam adipisicing qui sit laboris nisi elit cupidatat quis est sint pariatur consectetur. Veniam esse non tempor non nulla ullamco non consequat excepteur ex irure. Qui nostrud minim fugiat sunt. Amet do eiusmod veniam nisi Lorem. Aliquip ullamco pariatur dolor sunt laboris est in aliquip reprehenderit velit incididunt voluptate cillum. Minim do sint eu minim sunt dolore. Culpa deserunt fugiat non laboris id sint sint aliquip incididunt irure excepteur officia Lorem ea.",
-      "date": "January 15, 2019",
-      "category": {
-        "tag": "esse",
-        "color": "#63b598"
-      },
-      "links": {
-        "url": "https://www.google.com",
-        "text": "Read More"
-      }
-    },
-    {
       "text": "Ex ad veniam ut consectetur proident irure amet aliquip reprehenderit enim dolore. Irure et voluptate officia labore. Consequat velit anim labore ipsum velit incididunt.",
       "date": "January 22, 2017",
       "category": {
@@ -72,30 +60,6 @@ const data = [
       }
     },
     {
-      "text": "Elit incididunt est proident commodo. Ullamco duis id exercitation sunt ad cupidatat magna tempor deserunt cillum ipsum. Nostrud amet ipsum magna dolore laborum Lorem. Veniam pariatur labore non ut do sit proident occaecat aliquip consequat culpa officia et. Et magna tempor tempor id consequat ipsum. Excepteur ipsum amet est dolore laborum adipisicing esse id do adipisicing eiusmod cillum eiusmod incididunt.",
-      "date": "May 4, 2019",
-      "category": {
-        "tag": "non",
-        "color": "#1c0365"
-      },
-      "links": {
-        "url": "https://www.google.com",
-        "text": "Read More"
-      }
-    },
-    {
-      "text": "Id sunt sint quis nisi qui aliquip. Elit nulla sint reprehenderit fugiat ad fugiat dolor nulla. Deserunt incididunt sunt nostrud eu ad nisi nulla cillum. Et consectetur officia ullamco laborum dolor minim est deserunt non excepteur enim sit. Laborum dolore esse ullamco officia est officia labore enim duis ex dolore. Aliqua excepteur exercitation nostrud exercitation. Aliqua ut eu exercitation elit dolor cupidatat sit duis eiusmod tempor.",
-      "date": "September 3, 2018",
-      "category": {
-        "tag": "nisi",
-        "color": "#d2737d"
-      },
-      "links": {
-        "url": "https://www.google.com",
-        "text": "Read More"
-      }
-    },
-    {
       "text": "Id do duis ad excepteur excepteur cupidatat proident do. Voluptate quis consequat id irure in nisi qui reprehenderit adipisicing dolor laboris anim magna. Ex magna deserunt irure non. Aliquip sunt nostrud ullamco ea et enim enim pariatur. Enim voluptate ex pariatur et velit consequat aliquip consectetur ut cillum nulla dolor. Dolore culpa cupidatat anim Lorem enim minim aute Lorem veniam nulla nulla qui ad Lorem.",
       "date": "May 16, 2014",
       "category": {
@@ -132,47 +96,11 @@ const data = [
       }
     },
     {
-      "text": "Adipisicing veniam mollit eiusmod do sint adipisicing duis sit aliquip mollit qui sit. Id pariatur irure labore anim. In dolore dolore commodo quis. Reprehenderit excepteur voluptate consectetur dolor occaecat ea esse dolore aliqua velit labore ipsum mollit. Ullamco cillum aliqua dolore eu. Magna ullamco occaecat consequat minim in. Eu et occaecat do quis minim commodo ea pariatur sunt tempor incididunt voluptate.",
-      "date": "October 23, 2017",
-      "category": {
-        "tag": "sint",
-        "color": "#d2737d"
-      },
-      "links": {
-        "url": "https://www.google.com",
-        "text": "Read More"
-      }
-    },
-    {
-      "text": "Esse in eiusmod cupidatat ipsum officia. Ex amet exercitation nisi non veniam nostrud sunt do consectetur velit veniam. Reprehenderit nulla aliquip dolore aliquip duis proident officia reprehenderit incididunt consectetur aliquip eu. Occaecat ea ex veniam velit reprehenderit dolor reprehenderit consectetur minim id elit. Eiusmod nisi cupidatat esse ex id deserunt magna nisi est et magna esse cupidatat.",
-      "date": "April 6, 2014",
-      "category": {
-        "tag": "id",
-        "color": "#1c0365"
-      },
-      "links": {
-        "url": "https://www.google.com",
-        "text": "Read More"
-      }
-    },
-    {
       "text": "Qui mollit elit eiusmod fugiat et ullamco. Lorem labore occaecat labore irure. Eiusmod nostrud tempor proident deserunt adipisicing nostrud aute ex laboris.",
       "date": "December 30, 2016",
       "category": {
         "tag": "velit",
         "color": "#ce7d78"
-      },
-      "links": {
-        "url": "https://www.google.com",
-        "text": "Read More"
-      }
-    },
-    {
-      "text": "Laborum tempor sint enim officia sit id incididunt mollit ex ut. Ex mollit eiusmod amet non pariatur proident non est elit cupidatat. Esse reprehenderit proident minim consectetur pariatur nostrud dolore voluptate tempor nostrud do. Quis eu et proident fugiat aliqua velit amet et in non sint et. Sit aliqua proident et labore consequat cillum aute enim magna sunt labore esse.",
-      "date": "January 24, 2018",
-      "category": {
-        "tag": "aute",
-        "color": "#63b598"
       },
       "links": {
         "url": "https://www.google.com",
